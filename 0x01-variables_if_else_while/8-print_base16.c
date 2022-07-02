b298e7ff6d49b1adb7d34f8163dd84557240aeb1
njigu <harrismwaurah20@gmail.com>
2022-07-02 11:12:55 -0700

    corection base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,22 @@
+/*Program that prints all the numbers of base 16 in lowercase*/
+/*call header files*/
 #include <stdio.h>
-
-/**
- *main - Entry point
- *Description - Printing base 16 numbers
- *Return:0
- */
+/*Main-Entry point*/
+/* printing base 16 numbers*/
 int main(void)
 {
-    int i, y;
-    for (i = 0; i < 10; i++)
+	/*Declare variables*/
+	int  value1, value2;
+
+	/*Introduce for loop*/
+	for (value1 = 0; value1 < 10; value1++)
 	{
-	     putchar (i + '0');
+		putchar (value1 + '0');
 	}
-           for (y = 'a'; y <= 'f'; y++)
-               {
-	           putchar (y);
-               }	   
-        putchar('\n');
-        return (0);	   
+	for (value2 = 'a'; value2 <= 'f'; value2++)
+	{
+		putchar (value2);
+	}
+	putchar('\n');
+	return (0);
 }
