bad935efaf17d895f1597139306d1d4d2fb62807
njigu <harrismwaurah20@gmail.com>
2022-07-07 04:14:19 -0700

    test positive or negative for zeero

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+/*test the positive or negative function in main.h*/
+#include "main.h"
+/**
+ * main -main
+ * return: 0
+ */
+int main(void)
+{
+	/*declare variables*/
+	int i;
+	i = 0;
+
+	/*call the function in main.h*/
+	positive_or_negative(i);
+
+	return (0);
+}
+
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,6 @@
+/*define main header*/
+#ifndef MAIN_H
+#define MAIN_H
+#include<stdio.h>
+void positive_or_negative(int i);
+#endif
