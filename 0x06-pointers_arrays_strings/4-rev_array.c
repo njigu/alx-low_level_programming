d32305f5b2095017abd2a208dfcefc638c564610
njigu <harrismwaurah20@gmail.com>
2022-07-14 11:11:47 -0700

    quiz 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse the contents of an array
+ * @a: array
+ * @n: elements
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int h;
+
+	for (i = 0; i < (n / 2); i++)
+	{
+		h = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = h;
+	}
+}
