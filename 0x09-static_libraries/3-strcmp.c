f11b7d14053e80168a368c6150590c2446f3839e
cyborgNjigu <mwaura.njigu@gmail.com>
2022-07-22 20:45:22 +0300

    second attempt

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares strings
+ * @s1: string
+ * @s2: string
+ * Return: 0 for equal, neg or positive
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
