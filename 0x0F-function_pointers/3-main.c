897f47af6fd17deac2c902d3fd62c3118cb88da7
njigu <harrismwaurah20@gmail.com>
2022-08-04 12:10:00 -0700

    4th attempt

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,44 +1,49 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "3-calc.h"
-
 /**
- * main - Principal
- *
- * @ac: count arguments
- * @ag: array
- *
- * Return: Always 0
+ * main - get_op_func has operators correlated with
+ * func signs and funcs from op_func
+ * if not 4 arguments, return Error & exit 98
+ * if op is null, return Error & exit 99
+ * if div or mod 0, return Error & exit 100
+ * run calc, input one, operator, input two = pointer res to get_op
+ * @argc: arguments
+ * @argv: double pointer to arguments
+ * Return: 0
  */
-int main(int arc, char *arg[])
+int main(int argc, char *argv[])
 {
-	int num1, num2, (*operate)(int, int);
+	int one, two, ans;
+	int (*res)(int, int);
+	char *get_op;
 
-	if (arc != 4)
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if ((*arg[2] != '+' &&
-		*arg[2] != '-' &&
-		*arg[2] != '/' &&
-		*arg[2] != '*' &&
-		*arg[2] != '%')
-		|| arg[2][1] != '\0')
+
+	one = atoi(argv[1]);
+	two = atoi(argv[3]);
+	get_op = argv[2];
+
+	/* added edge case if argv[2] was longer than 1 char*/
+	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	num1 = atoi(arg[1]);
-	num2 = atoi(arg[3]);
-	if ((*arg[2] == '/' || *arg[2] == '%') && (num2 == 0))
+	if ((*get_op == '/' || *get_op == '%') && (*argv[3] == '0'))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	operate = get_op_func(arg[2]);
-	printf("%d\n", operate(num1, num2));
-	return (0);
 
+	res = get_op_func(get_op);
+	ans = res(one, two);
+
+	printf("%d\n", ans);
+	return (0);
 }
